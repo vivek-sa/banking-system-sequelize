cfc6347d3374beecab13028e392c6f445a95b651
macbookOS <mac@vivek.local>
2023-10-12 15:12:51 +0530

    modify: change logging in config

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: "postgres",
-    logging: "true",
+    logging: (...msg) => console.log(msg),
   },
   test: {
     username: process.env.DB_USERNAME,
